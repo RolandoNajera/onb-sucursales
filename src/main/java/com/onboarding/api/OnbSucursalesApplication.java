66c0a3ce5076788806cc6125f0260c30ff054de1
Rolando Mac <erolando.najera@gmail.com>
2018-11-19 11:40:20 -0600

    Commit Inicial

---
+++ src/main/java/com/onboarding/api/OnbSucursalesApplication.java
@@ -0,0 +1,12 @@
+package com.onboarding.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnbSucursalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnbSucursalesApplication.class, args);
+	}
+}
